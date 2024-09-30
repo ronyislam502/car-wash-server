53ab7c836e9b5ce1274ed292e99d6131ef5df6e7
ronyislam502 <ronyislam502@gmail.com>
2024-09-30 17:23:09 +0600

    project setup and error handle

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1", router);
+
+const getController = (req: Request, res: Response) => {
+  res.send("car-wash app");
+};
+
+app.get("/", getController);
+app.use(globalErrorHandler);
+app.use(notFound);
+
+// console.log(process.cwd());
+
+export default app;
